50f364449d3995ced5b62455dc12d0446c2dde3b
Cotangente00 <julianavila458@gmail.com>
2024-06-05 17:02:19 -0500

    update 5/06/2024 nuevos modelos agregados

--- BEEL/Sistema_autenticacion_registro/models.py
+++ BEEL/Sistema_autenticacion_registro/models.py
@@ -14,16 +14,17 @@ class postulantes(models.Model):
     celular = models.CharField(max_length=255, verbose_name="Celular", null=False)
     correo = models.EmailField(verbose_name="Correo electrónico", unique=True, null=False)
     direccion = models.CharField(max_length=255, verbose_name="Dirección", null=False)
-    descripcionDiscapacidad = models.TextField(verbose_name="Descripción de discapacidad", null=False)
+    descripcion_discapacidad = models.TextField(verbose_name="Descripción de discapacidad", null=False)
     contrasena = models.CharField(max_length=255, verbose_name="Contraseña", null=False)
     confirmacion_contrasena = models.CharField(max_length=255, verbose_name="Confirmación de contraseña", null=False)
     nacionalidad = models.CharField(max_length=255, verbose_name="Nacionalidad", null=False)
-
+   
 
     def save(self, *args, **kwargs):
         if not self.id:
             self.contrasena = make_password(self.contrasena)
         super().save(*args, **kwargs)
+    pass
 
 class empresas(models.Model):
     id = models.AutoField(primary_key=True)
@@ -41,19 +42,29 @@ class empresas(models.Model):
         if not self.id:
             self.contrasena = make_password(self.contrasena)
         super().save(*args, **kwargs)
+    pass
 
-
-class tipoDocumentos(models.Model):
+class tipo_documentos(models.Model):
     id = models.AutoField(primary_key=True)
     abreviatura = models.CharField(max_length=255, verbose_name="Abreviatura", null=False)
     descripcion = models.CharField(max_length=255, verbose_name="descripcion", null=False)
+    pass
 
 
 class localidad(models.Model):
     id = models.AutoField(primary_key=True)
     localidades = models.CharField(max_length=255, verbose_name="localidades", null=False)
+    pass
 
 
-class tipoDiscapacidad(models.Model):
+class tipo_discapacidad(models.Model):
     id = models.AutoField(primary_key=True)
-    tipo = models.CharField(max_length=255, verbose_name="localidades", null=False)
+    tipo = models.CharField(max_length=255, verbose_name="tipo", null=False)
+    pass
+
+
+class sexo(models.Model):
+    id = models.AutoField(primary_key=True)
+    sexo = models.CharField(max_length=255, verbose_name="sexo", null=False)
+    pass
+
