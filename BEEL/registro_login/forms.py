0b291a1a40b9f91eefe2cb7834817e3813e72387
Cotangente <julianavila458@gmail.com>
2024-06-25 22:20:20 -0500

    mensage de estado de postulación mensage en el header

--- BEEL/registro_login/forms.py
+++ BEEL/registro_login/forms.py
@@ -16,8 +16,9 @@ class RegistroForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super(RegistroForm, self).__init__(*args, **kwargs)
-        self.fields['num1'].initial = random.randint(1, 10)
-        self.fields['num2'].initial = random.randint(1, 10)
+        if not self.is_bound:
+            self.fields['num1'].initial = random.randint(1, 10)
+            self.fields['num2'].initial = random.randint(1, 10)
         self.fields['resultado'].label = f'¿Cuánto es {self.fields["num1"].initial} + {self.fields["num2"].initial}?'
 
     def clean_resultado(self):
